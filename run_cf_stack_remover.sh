985f8aa1e08c546ea387988faa907217ee3a5fea
William Jia <wjia@WJia-mac.ott.signiant.com>
2018-12-20 16:05:29 -0500

    further enhancement to Dockerfile and stackremover.py file

--- run_cf_stack_remover.sh
+++ run_cf_stack_remover.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+#this script was created to execute the cf_delete_stacks.py in multiple regions
+# to execute the script execute 'sh run_cf_stack_remover.sh [region1] [region2] ...' 
 
 if [ "$#" -lt 1 ]; then
     echo "add argument '--help' or AWS region such as 'us-west-2'"
 elif [ "$1" == "--help" ]; then
-    python3 cf_delete_stacks.py --help
+    echo "this script was created to execute the cf_delete_stacks.py in multiple regions"
+    echo "to execute the script execute 'sh run_cf_stack_remover.sh [region1] [region2] ...'"
 else
     for item in "$@"; do
         python3 cf_delete_stacks.py --stack-status ROLLBACK_COMPLETE --region $item
